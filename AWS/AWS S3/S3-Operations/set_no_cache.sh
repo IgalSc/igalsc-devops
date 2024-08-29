914e7fc0cb47559214ed0932cf01bf84944ed9a6
IgalSc <igalsc@gmail.com>
2024-08-29 00:29:28 -0400

    Update set_no_cache.sh

--- AWS/AWS S3/S3-Operations/set_no_cache.sh
+++ AWS/AWS S3/S3-Operations/set_no_cache.sh
@@ -1,1 +1,15 @@
-.
+#!/bin/bash
+
+# Set your AWS profile, region, and bucket name
+PROFILE="profile_name"
+REGION="us-east-1"
+BUCKET="bucket_name"
+
+aws s3 mv ./ $BUCKET \
+  --profile "$PROFILE" \
+  --region $REGION \
+  --recursive \
+  --cache-control no-cache,no-store,must-revalidate,public
+
+
+echo "Run complete."
